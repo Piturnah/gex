618044ccda69850841a802dedaec34a78a0a1a54
Piturnah <peterhebden6@gmail.com>
2022-08-09 21:26:11 +0100

    Implement state switching and `Branch` state
    Switch to Branch state with `b`, exit with `Esc`

---
+++ src/branch.rs
@@ -0,0 +1,51 @@
+use crossterm::{cursor, style::Attribute};
+use std::{fmt, process::Command};
+
+pub struct BranchList {
+    pub branches: Vec<String>,
+    pub cursor: usize,
+}
+
+impl fmt::Display for BranchList {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        for (i, branch) in self.branches.iter().enumerate() {
+            if i == self.cursor {
+                write!(
+                    f,
+                    "{}{}{}{}\n",
+                    cursor::MoveToColumn(0),
+                    Attribute::Reverse,
+                    branch,
+                    Attribute::Reset
+                )?;
+            } else {
+                write!(f, "{}{}\n", cursor::MoveToColumn(0), branch)?;
+            }
+        }
+        Ok(())
+    }
+}
+
+impl BranchList {
+    pub fn new() -> Self {
+        let mut branch_list = Self {
+            branches: Vec::new(),
+            cursor: 0,
+        };
+        branch_list.fetch();
+        branch_list
+    }
+
+    pub fn fetch(&mut self) {
+        let branches = Command::new("git")
+            .arg("branch")
+            .output()
+            .expect("failed to run `git branch`");
+
+        self.branches = std::str::from_utf8(&branches.stdout)
+            .expect("broken stdout from `git branch`")
+            .lines()
+            .map(|l| l.to_string())
+            .collect::<Vec<_>>();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,21 @@ use std::{
     process::{self, Command, Stdio},
 };
 
+mod branch;
 pub mod parse;
 mod status;
 
+use branch::BranchList;
 use status::Status;
 
 pub trait Expand {
     fn toggle_expand(&mut self);
     fn expanded(&self) -> bool;
+}
+
+enum State {
+    Status,
+    Branch,
 }
 
 fn main() {
@@ -36,68 +43,111 @@ fn main() {
     }
 
     let mut status = Status::new();
+    let mut branch_list = BranchList::new();
+
     crossterm::execute!(stdout(), terminal::EnterAlternateScreen)
         .expect("failed to enter alternate screen");
     terminal::enable_raw_mode().expect("failed to put terminal in raw mode");
     print!("{}", cursor::Hide);
+
+    let mut state = State::Status;
+
     loop {
-        println!(
-            "{}{}{}{}",
-            cursor::MoveToRow(0),
-            terminal::Clear(ClearType::All),
-            status,
-            cursor::MoveToColumn(0)
-        );
-        match event::read().unwrap() {
-            Event::Key(event) => match event.code {
-                KeyCode::Char('j') | KeyCode::Down => status.down(),
-                KeyCode::Char('k') | KeyCode::Up => status.up(),
-                KeyCode::Char('s') => status.stage(),
-                KeyCode::Char('S') => {
-                    Command::new("git")
-                        .args(["add", "."])
-                        .output()
-                        .expect("couldn't run `git add .`");
-                    status.fetch();
-                }
-                KeyCode::Char('u') => status.unstage(),
-                KeyCode::Char('U') => {
-                    Command::new("git")
-                        .arg("reset")
-                        .output()
-                        .expect("failed to run `git reset`");
-                    status.fetch();
-                }
-                KeyCode::Tab => status.expand(),
-                KeyCode::Char('c') => {
-                    crossterm::execute!(stdout(), terminal::LeaveAlternateScreen)
+        match state {
+            State::Status => {
+                print!(
+                    "{}{}{}{}",
+                    cursor::MoveToRow(0),
+                    terminal::Clear(ClearType::All),
+                    status,
+                    cursor::MoveToColumn(0)
+                );
+            }
+            State::Branch => {
+                print!(
+                    "{}{}{}",
+                    cursor::MoveToRow(0),
+                    terminal::Clear(ClearType::All),
+                    branch_list
+                );
+            }
+        }
+        if let Event::Key(event) = event::read().unwrap() {
+            match state {
+                State::Status => match event.code {
+                    KeyCode::Char('j') | KeyCode::Down => status.down(),
+                    KeyCode::Char('k') | KeyCode::Up => status.up(),
+                    KeyCode::Char('s') => status.stage(),
+                    KeyCode::Char('S') => {
+                        Command::new("git")
+                            .args(["add", "."])
+                            .output()
+                            .expect("couldn't run `git add .`");
+                        status.fetch();
+                    }
+                    KeyCode::Char('u') => status.unstage(),
+                    KeyCode::Char('U') => {
+                        Command::new("git")
+                            .arg("reset")
+                            .output()
+                            .expect("failed to run `git reset`");
+                        status.fetch();
+                    }
+                    KeyCode::Tab => status.expand(),
+                    KeyCode::Char('c') => {
+                        crossterm::execute!(stdout(), terminal::LeaveAlternateScreen)
+                            .expect("failed to leave alternate screen");
+                        Command::new("git")
+                            .arg("commit")
+                            .stdout(Stdio::inherit())
+                            .stdin(Stdio::inherit())
+                            .stderr(Stdio::inherit())
+                            .output()
+                            .expect("failed to run `git commit`");
+                        status.fetch();
+                        crossterm::execute!(stdout(), terminal::EnterAlternateScreen, cursor::Hide)
+                            .expect("failed to enter alternate screen");
+                    }
+                    KeyCode::Char('b') => state = State::Branch,
+                    KeyCode::Char('r') => status.fetch(),
+                    KeyCode::Char('q') => {
+                        terminal::disable_raw_mode().unwrap();
+                        crossterm::execute!(
+                            stdout(),
+                            terminal::LeaveAlternateScreen,
+                            cursor::Show,
+                            cursor::MoveToColumn(0)
+                        )
                         .expect("failed to leave alternate screen");
-                    Command::new("git")
-                        .arg("commit")
-                        .stdout(Stdio::inherit())
-                        .stdin(Stdio::inherit())
-                        .stderr(Stdio::inherit())
-                        .output()
-                        .expect("failed to run `git commit`");
-                    status.fetch();
-                    crossterm::execute!(stdout(), terminal::EnterAlternateScreen, cursor::Hide)
-                        .expect("failed to enter alternate screen");
-                }
-                KeyCode::Char('r') => status.fetch(),
-                KeyCode::Char('q') | KeyCode::Esc => {
-                    terminal::disable_raw_mode().unwrap();
-                    crossterm::execute!(
-                        stdout(),
-                        terminal::LeaveAlternateScreen,
-                        cursor::Show,
-                        cursor::MoveToColumn(0)
-                    )
-                    .expect("failed to leave alternate screen");
-                    process::exit(0);
-                }
-                _ => {}
-            },
-            _ => {}
+                        process::exit(0);
+                    }
+                    _ => {}
+                },
+                State::Branch => match event.code {
+                    KeyCode::Char('k') | KeyCode::Up => {
+                        branch_list.cursor = branch_list.cursor.checked_sub(1).unwrap_or(0);
+                    }
+                    KeyCode::Char('j') | KeyCode::Down => {
+                        branch_list.cursor += 1;
+                        if branch_list.cursor >= branch_list.branches.len() {
+                            branch_list.cursor = branch_list.branches.len() - 1;
+                        }
+                    }
+                    KeyCode::Esc => state = State::Status,
+                    KeyCode::Char('q') => {
+                        terminal::disable_raw_mode().unwrap();
+                        crossterm::execute!(
+                            stdout(),
+                            terminal::LeaveAlternateScreen,
+                            cursor::Show,
+                            cursor::MoveToColumn(0)
+                        )
+                        .expect("failed to leave alternate screen");
+                        process::exit(0);
+                    }
+                    _ => {}
+                },
+            };
         }
     }
 }
